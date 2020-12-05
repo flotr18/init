8476a0f7e6a919b7dae0d04321cb4eefeb3527cb
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-12-05 20:46:30 +0000

    Initial COmmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    // the form fields are stored in a state
+    this.state = {
+      firstname: '',
+      lastname: '',
+      mail: '',
+      message: ''
+    };
+
+    //this binding is necessary to make `this` work in the callback
+    //generally, if you refer to a method without () after it, such as onClick={this.handleClick}, you should bind that method
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  //once the input boxes are changed, update the state to match the value
+  handleChange(event) {
+    //name of the input boxes must match the property names in the state
+    const name = event.target.name;
+    const value = event.target.value;
+
+
+    this.setState(
+
+        {
+          [name]: value,
+
+        });
+  }
+
+  handleSubmit(event) {
+    //preventDefault() is called on the event when it occurs to prevent a browser reload/refresh
+    event.preventDefault();
+
+    //use axios to send a POST request to the server which includes the state information for the new user to be created
+    axios.post('/', this.state)
+        //on success go to home
+        .then(res => this.props.history.push('/'))
+        .catch(error => {
+          console.log(error);
+        });
+  }
+
+  render(){
+    return (
+        <div>
+          <p className="form_title h1 text-center">CONTACT US</p>
+          <form onSubmit={this.handleSubmit}>
+
+            <div className="container-fluid text-center w-75 form_contact">
+              <div className="row mt-3">
+                <div className="col-6">
+                  <input className={"contact_input w-50 mt-5"} placeholder={"firstname"} name={"firstname"} value={this.state.firstname} onChange={this.handleChange} />
+
+                </div>
+                <div className="col-6">
+                  <input className={"contact_input w-50 mt-5"} placeholder={"lastname"}  name={"lastname"} value={this.state.lastname} onChange={this.handleChange}/>
+
+                </div>
+              </div>
+
+              <div className="row mt-5">
+                <div className="col-12 email">
+                  <input className={"contact_input w-75 mt-3"} placeholder={"Email"} name={"mail"} value={this.state.mail} onChange={this.handleChange}/>
+
+                </div>
+              </div>
+              <div className="row mt-5">
+                <div className="col-12 message">
+                  <textarea className={"contact_input-message w-75 mt-3"} placeholder={"Message"}  name={"message"} value={this.state.message} onChange={this.handleChange}/>
+
+                </div>
+              </div>
+              <div className="row mt-5">
+                <div className="col-12 submit">
+                  <input className="submit-button w-25" type="submit" value="Submit" />
+
+                </div>
+              </div>
+            </div>
+          </form>
+
+          <div className={"container-fluid text-center w-75"}>
+            <div className={"row"}>
+              <div className={"col disclaimer"}>
+                <p className={"h6 disclaimer-message"}>DISCLAIMER/MESSAGE</p>
+              </div>
+
+            </div>
+          </div>
+
+
+
+        </div>
+
+    )
+
+  }
+
+
+
 }
 
 export default App;
---
+++ src/server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+
+const server = express();
+// the value for dbname should match your database name
+const dbname = 'asperger';
+
+server.use(express.static('dist'));
+
+
+
+const dbroute = process.env.MONGODB_URL || "mongodb+srv://admin:root@cluster0.elh7n.mongodb.net/test?retryWrites=true&w=majority";
+
+let db;
+
+MongoClient.connect(dbroute, (err, client) => {
+    if (err) throw err;
+
+
+    db = client.db(dbname);
+    server.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}!`));
+    console.log('Connected !')
+});
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+server.post('/', (req, res) => {
+
+
+    db.collection('contact').insertOne(req.body, (err, result) => {
+        if (err) throw err;
+
+        console.log('created in database');
+        res.redirect('/');
+    });
+});
+
