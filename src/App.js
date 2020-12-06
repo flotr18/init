64cff9a3313c0c6cb15e7ee093d61dd2d007c11d
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-12-06 00:25:42 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -34,13 +34,12 @@ class App extends Component {
     event.preventDefault();
 
     //use axios to send a POST request to the server which includes the state information for the new user to be created
-    axios
-      .post("/", this.state)
-      //on success go to home
-      .then((res) => this.props.history.push("/"))
-      .catch((error) => {
-        console.log(error);
-      });
+    axios.post('/http://localhost:3000/', this.state)
+        //on success go to home
+        .then(res => this.props.history.push('/'))
+        .catch(error => {
+          console.log(error);
+        });
   }
 
   render() {
