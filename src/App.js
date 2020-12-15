cf2d2420b80afc4904c49ff492735772e93906e9
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-12-15 05:12:15 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
     //use axios to send a POST request to the server which includes the state information for the new user to be created
 
     axios
-        .post("http://localhost:5000/", this.state)
+        .post("https://jackie42.herokuapp.com/", this.state)
         //on success go to home
         .then((res) => this.props.history.push("/"))
         .catch((error) => {
