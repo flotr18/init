52b5edb32ec3a191b7e46da0c737a1d1e27553d4
KhoiNguyen <thorwaitson@gmail.com>
2020-12-15 15:30:17 -0600

    koi update

--- src/App.js
+++ src/App.js
@@ -22,105 +22,89 @@ class App extends Component {
     this.setState({
       [name]: value,
     });
-
-
   }
 
   handleSubmit(event) {
     event.preventDefault();
 
-    if (window.location.href === 'http://localhost:3000/'){
+    if (window.location.href === "localhost") {
       axios
-          .post("http://localhost:5000", this.state)
-          .then((res) => this.props.history.push("/"))
-          .catch((error) => {
-            console.log(error);
-          });
-
-    }
-
-    else {
+        .post("http://localhost:5000", this.state)
+        .then((res) => this.props.history.push("/"))
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
       axios
-          .post("/", this.state)
-          .then((res) => this.props.history.push("/"))
-          .catch((error) => {
-            console.log(error);
-          });
-
+        .post("/", this.state)
+        .then((res) => this.props.history.push("/"))
+        .catch((error) => {
+          console.log(error);
+        });
     }
-
-
-
-
-
-
-
-
   }
 
   render() {
     return (
-        <div>
-          <p className="form_title h1 text-center">CONTACT US</p>
-          <form onSubmit={this.handleSubmit}>
-            <div className="container-fluid text-center w-75 form_contact">
-              <div className="row mt-3">
-                <div className="col-6">
-                  <input
-                      className={"contact_input w-50 mt-5"}
-                      placeholder={"firstname"}
-                      name={"firstname"}
-                      value={this.state.firstname}
-                      onChange={this.handleChange}
-                  />
-                </div>
-                <div className="col-6">
-                  <input
-                      className={"contact_input w-50 mt-5"}
-                      placeholder={"lastname"}
-                      name={"lastname"}
-                      value={this.state.lastname}
-                      onChange={this.handleChange}
-                  />
-                </div>
-              </div>
-
-              <div className="row mt-5">
-                <div className="col-12 email">
-                  <input
-                      className={"contact_input w-75 mt-3"}
-                      placeholder={"Email"}
-                      name={"mail"}
-                      value={this.state.mail}
-                      onChange={this.handleChange}
-                  />
-                </div>
-              </div>
-              <div className="row mt-5">
-                <div className="col-12 message">
-                <textarea
-                    className={"contact_input-message w-75 mt-3"}
-                    placeholder={"Message"}
-                    name={"message"}
-                    value={this.state.message}
-                    onChange={this.handleChange}
+      <div>
+        <p className="form_title h1 text-center">CONTACT US</p>
+        <form onSubmit={this.handleSubmit}>
+          <div className="container-fluid text-center w-75 form_contact">
+            <div className="row mt-3">
+              <div className="col-6">
+                <input
+                  className={"contact_input w-50 mt-5"}
+                  placeholder={"firstname"}
+                  name={"firstname"}
+                  value={this.state.firstname}
+                  onChange={this.handleChange}
                 />
-                </div>
               </div>
-              <div className="row mt-5">
-                <div className="col-12 submit">
-                  <input
-                      className="submit-button w-25"
-                      type="submit"
-                      value="Submit"
-                  />
-                </div>
+              <div className="col-6">
+                <input
+                  className={"contact_input w-50 mt-5"}
+                  placeholder={"lastname"}
+                  name={"lastname"}
+                  value={this.state.lastname}
+                  onChange={this.handleChange}
+                />
               </div>
             </div>
-          </form>
 
-
-        </div>
+            <div className="row mt-5">
+              <div className="col-12 email">
+                <input
+                  className={"contact_input w-75 mt-3"}
+                  placeholder={"Email"}
+                  name={"mail"}
+                  value={this.state.mail}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="row mt-5">
+              <div className="col-12 message">
+                <textarea
+                  className={"contact_input-message w-75 mt-3"}
+                  placeholder={"Message"}
+                  name={"message"}
+                  value={this.state.message}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="row mt-5">
+              <div className="col-12 submit">
+                <input
+                  className="submit-button w-25"
+                  type="submit"
+                  value="Submit"
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     );
   }
 }
